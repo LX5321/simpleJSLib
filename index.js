8642a835a064988638a88fcad8ef86050b21aeda
lx5321 <alexanderwebsite5321@gmail.com>
2022-03-05 13:31:05 +0530

    Edited index.js

--- index.js
+++ index.js
@@ -3,11 +3,12 @@
  * 	for testing
  *
  * 	*/
+
 function init (){return "Its Working!";}
 
 function sub (a,b){
-if(a>b) return a-b;
-return b-a;
+    if(a>b) return a-b;
+    return b-a;
 }
 
 function product (a,b){return a*b;}
