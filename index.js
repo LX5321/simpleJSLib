8cc999b435dbfac395764af732399187d2cf106f
sneha <parabsneha19@gmail.com>
2022-03-05 12:58:59 +0530

    sum and product function added

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-/**
+	/**
  * 	Simple Js Lib
  * 	for testing
  *
  * 	*/
 function init (){return "Its Working!";}
+
+function sub (a,b){
+return a-b;
+}
+
+function product (a,b){
+return a*b;
+}
